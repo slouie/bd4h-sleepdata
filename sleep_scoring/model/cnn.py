de0e47e0645392425200a9bf5c196fdafb624869
Steven Louie <slouiecs@gmail.com>
2018-11-16 23:47:07 +0000

    full tsinalis cnn

--- sleep_scoring/model/cnn.py
+++ sleep_scoring/model/cnn.py
@@ -16,14 +16,20 @@ class CNN(nn.Module):
 		# 	pool2: 	max pool size 10 stride 2
 		# 	fc1: 	500 units
 		# 	fc2: 	500 units
-		# The full thing is too large for me to test right now, used smaller filter
-		# sizes and fc layers. It's still very dense and can take up to 1s per batch (no gpu).
 		self.conv1 = nn.Conv1d(in_channels=1, out_channels=20, kernel_size=200)
 		self.pool1 = nn.MaxPool1d(kernel_size=20, stride=10)
-		self.conv2 = nn.Conv2d(1, 64, kernel_size=(20,30))
-		self.pool2 = nn.MaxPool2d(kernel_size=(1,20), stride=(1,4))
-		self.fc1 = nn.Linear(in_features=64*433, out_features=64)
-		self.fc2 = nn.Linear(64, 6)
+		self.conv2 = nn.Conv2d(1, 400, kernel_size=(20,30))
+		self.pool2 = nn.MaxPool2d(kernel_size=(1,10), stride=(1,2))
+		self.fc1 = nn.Linear(in_features=400*871, out_features=500)
+		self.fc2 = nn.Linear(500, 6)
+
+		# Smaller version of above
+		# self.conv1 = nn.Conv1d(in_channels=1, out_channels=20, kernel_size=200)
+		# self.pool1 = nn.MaxPool1d(kernel_size=20, stride=10)
+		# self.conv2 = nn.Conv2d(1, 64, kernel_size=(20,30))
+		# self.pool2 = nn.MaxPool2d(kernel_size=(1,20), stride=(1,4))
+		# self.fc1 = nn.Linear(in_features=64*433, out_features=64)
+		# self.fc2 = nn.Linear(64, 6)
 
 		# self.conv1 = nn.Conv1d(in_channels=6, out_channels=6, kernel_size=5)
 		# self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
@@ -35,7 +41,8 @@ class CNN(nn.Module):
 		x = self.pool1(F.relu(self.conv1(x)))
 		x = x.view(-1, 1, 20, 1779)
 		x = self.pool2(F.relu(self.conv2(x)))
-		x = x.view(-1, 64*433)
+		x = x.view(-1, 400*871)
+		#x = x.view(-1, 64*433)
 		x = F.relu(self.fc1(x))
 		x = self.fc2(x)
 		return x
